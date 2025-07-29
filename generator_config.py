c6d07d27fb37949ec38970e0300312c243bdfb66
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2025-07-29 22:22:22 +0300

    Update generator_config.py Home Work

--- generator_config.py
+++ generator_config.py
@@ -1,1 +1,7 @@
+app_name = input("Enter App Name :: ")
+db_ip = input("Enter IP :: ")
+db_port = input("Enter Port :: ")
 
+print(f"Connecting App: {app_name} to Database at {db_ip}:{db_port}...")
+
+# input [ Connecting App: WebApp1 to Database at 10.0.0.5:5432... ]
